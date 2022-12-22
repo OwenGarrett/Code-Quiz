52bd0b0672c21b0b7818cf35255e149835a09dcb
Owen Garrett <owengarrett@Owens-Air.lan>
2022-12-22 13:13:46 -0500

    added js  structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
-console.log("Hello World")
+const questions = [
+    {
+        question: "What color is the sky?", 
+        answer: ["Blue", "Red", "Green"] ,
+        correctAnswer: "Blue" ,
+
+    
+    },
+    
+    {
+        question: "What color is the grass?", 
+        answer: ["Blue", "Red", "Green"] ,
+        correctAnswer: "Green" ,
+
+    },
+     
+    {
+        question: "Who is the coolest in DBZ?", 
+        answer: ["Piccolo", "Goku", "Charmander"] ,
+        correctAnswer: "Piccolo" ,
+
+    }
+]
+console.log(questions[1].correctAnswer)
+
+
+var startButton= document.querySelector(".startButton")
+    startButton.addEventListener('click', function(){
+       console.log("Hello World"); 
+       startButton.style.display = "none";
+       
+    })
